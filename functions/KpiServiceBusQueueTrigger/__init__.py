d61baf95437f8eebe5a680fb469391a3fcd9fe20
Bartlomiej <Bartlomiej>
2023-01-04 23:24:16 +0100

    updated azure function

--- functions/KpiServiceBusQueueTrigger/__init__.py
+++ functions/KpiServiceBusQueueTrigger/__init__.py
@@ -10,15 +10,16 @@ def main(msg: func.ServiceBusMessage):
     msg = json.loads(msg.get_body().decode('utf-8'))
     logging.info('Python ServiceBus queue trigger processed message'
                  )
-    if msg['Good Count percentage'] < 0.9:
+    if float(msg['Good Count percentage']) < 0.9:
         device_id = msg['ConnectionDeviceId']
         registry_manager = IoTHubRegistryManager(os.getenv('CONN_STRING'))
         twin = registry_manager.get_twin(device_id)
         production_rate = twin.properties.as_dict()[
             'reported']['ProductionRate']
+
         patch = {
             "properties": {
-                "desired": {"ProductionRate": production_rate}
+                "desired": {"ProductionRate": production_rate - 10}
             }
         }
         registry_manager.update_twin(device_id, patch)
